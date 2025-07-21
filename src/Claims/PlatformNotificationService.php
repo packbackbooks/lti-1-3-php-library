f42094e8d6caf024ce318e2b9e9124f7288de4b0
Davo Hynds <dbhynds@gmail.com>
2025-07-21 14:02:39 -0500

    use claims

---
+++ src/Claims/PlatformNotificationService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Packback\Lti1p3\Claims;
+
+class PlatformNotificationService
+{
+    public static function key(): string
+    {
+        return LtiConstants::PNS_CLAIM_SERVICE;
+    }
+
+    public function platformNotificationServiceUrl(): string
+    {
+        return $this->getBody()['platform_notification_service_url'];
+    }
+
+    public function serviceVersions(): array
+    {
+        return $this->getBody()['service_versions'];
+    }
+
+    public function scope(): array
+    {
+        return $this->getBody()['scope'];
+    }
+
+    public function noticeTypesSupported(): array
+    {
+        return $this->getBody()['notice_types_supported'];
+    }
+
+    public function supportsNoticeType(string $noticeType): bool
+    {
+        return in_array($noticeType, $this->noticeTypesSupported());
+    }
+
+    public function hasScope(string $requiredScope): bool
+    {
+        return in_array($requiredScope, $this->scope());
+    }
+}
---
+++ src/Factories/ClaimFactory.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Packback\Lti1p3\Factories;
+
+use Packback\Lti1p3\Claims\Activity;
+use Packback\Lti1p3\Claims\PlatformNotificationService;
+use Packback\Lti1p3\LtiConstants;
+use Packback\Lti1p3\Messages\LtiMessage;
+use Packback\Lti1p3\Messages\Notice;
+
+abstract class Factory
+{
+    public static function create(string $claim, LtiMessage $message): Claim
+    {
+        $class = $this->getClaimClass($claim);
+
+        return new $class($message->getBody());
+    }
+
+    public function getClaimClass(string $claim): string
+    {
+        $typeClaimMap = [
+            // LtiConstants::VERSION => ::class,
+            // LtiConstants::DEPLOYMENT_ID => ::class,
+            // LtiConstants::ROLES => ::class,
+            // LtiConstants::FOR_USER => ::class,
+            // LtiConstants::MESSAGE_TYPE => ::class,
+            // LtiConstants::TARGET_LINK_URI => ::class,
+            // LtiConstants::RESOURCE_LINK => ::class,
+            // LtiConstants::CONTEXT => ::class,
+            // LtiConstants::CUSTOM => ::class,
+            // LtiConstants::LAUNCH_PRESENTATION => ::class,
+            // LtiConstants::LIS => ::class,
+            // LtiConstants::LTI1P1 => ::class,
+            // LtiConstants::ROLE_SCOPE_MENTOR => ::class,
+            // LtiConstants::TOOL_PLATFORM => ::class,
+            // LtiConstants::DL_CONTENT_ITEMS => ::class,
+            // LtiConstants::DL_DATA => ::class,
+            // LtiConstants::DL_DEEP_LINK_SETTINGS => ::class,
+            // LtiConstants::NRPS_CLAIM_SERVICE => ::class,
+            // LtiConstants::AGS_CLAIM_ENDPOINT => ::class,
+            // LtiConstants::GS_CLAIM_SERVICE => ::class,
+            LtiConstants::PNS_CLAIM_SERVICE => PlatformNotificationService::class,
+            // LtiConstants::PNS_CLAIM_NOTICE => ::class,
+            // LtiConstants::AP_CLAIM_SERVICE => ::class,
+            // LtiConstants::AP_CLAIM_REPORT => ::class,
+            LtiConstants::AP_CLAIM_ACTIVITY => Activity::class,
+            // LtiConstants::AP_CLAIM_SUBMISSION => ::class,
+            // LtiConstants::AP_CLAIM_REPORT_TYPE => ::class,
+            // LtiConstants::AP_CLAIM_ASSET => ::class,
+            // LtiConstants::EULA_CLAIM_SERVICE => ::class,
+        ];
+
+        return $typeClaimMap[$claim];
+    }
+}
