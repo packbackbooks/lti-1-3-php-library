5c4ef6ece5518f3314e4a44e9b78b873fa73fb36
Davo Hynds <dbhynds@gmail.com>
2025-07-23 11:04:50 -0500

    Build out notice claim

--- src/Claims/Notice.php
+++ src/Claims/Notice.php
@@ -2,8 +2,11 @@
 
 namespace Packback\Lti1p3\Claims;
 
+use Packback\Lti1p3\Claims\Concerns\HasId;
+
 class Notice extends Claim
 {
+    use HasId;
     public static function claimKey(): string
     {
         return Claim::NOTICE;
@@ -12,5 +15,15 @@ class Notice extends Claim
     public function sub()
     {
         return $this->getBody()['sub'] ?? null;
+    }
+
+    public function type(): string
+    {
+        return $this->getBody()['type'];
+    }
+
+    public function timestamp(): string
+    {
+        return $this->getBody()['timestamp'];
     }
 }
--- src/Messages/Notice.php
+++ src/Messages/Notice.php
@@ -2,6 +2,7 @@
 
 namespace Packback\Lti1p3\Messages;
 
+use Packback\Lti1p3\Claims\Notice as NoticeClaim;
 use Packback\Lti1p3\MessageValidators\NoticeMessageValidator;
 
 class Notice extends LtiMessage
@@ -14,5 +15,10 @@ class Notice extends LtiMessage
     public static function messageValidator(): string
     {
         return NoticeMessageValidator::class;
+    }
+
+    public function noticeClaim(): NoticeClaim
+    {
+        return NoticeClaim::create($this);
     }
 }
